a1cb18b3261da2b19f65a2153dfa5ff8ff92abe0
cotto89 <cottto89@gmail.com>
2016-10-29 22:31:34 +0900

    Update CounterApp and build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,2 @@
 export * from "./lib/UseCase";
-export * from "./lib/compose";
 export * from "./lib/Dispatcher";
--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,5 @@ function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
 __export(require("./lib/UseCase"));
-__export(require("./lib/compose"));
 __export(require("./lib/Dispatcher"));
 //# sourceMappingURL=index.js.map
